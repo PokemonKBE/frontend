107e1789df3b298298c41049cc30fa285e8efa43
Melisa <melcafidan@gmail.com>
2022-09-12 14:45:00 +0200

    clean-up

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,19 @@
 <template>
   <v-app>
     <v-main>
-      <BarView />
       <router-view />
+      <BarView />
     </v-main>
   </v-app>
 </template>
 
 <script lang="ts">
 import {defineComponent} from 'vue'
-import HomeView from "@/views/HomeView.vue";
 import BarView from "@/views/BarView.vue";
 
 export default defineComponent({
   name: 'App',
   components: {
-    HomeView,
     BarView
   },
   data() {
---
+++ src/views/BuyCardsView.vue
@@ -0,0 +1,72 @@
+<template>
+  <v-container class="picture" fluid style="height: 100%;">
+
+    <div class="pokiCard">
+      <v-card
+          class="mx-auto"
+          width="1200"
+          height="400">
+
+        <template v-slot:title>
+          This is a title
+        </template>
+
+        <v-card-text>
+          This is content
+        </v-card-text>
+      </v-card>
+    </div>
+
+    <div class="pokiCard">
+      <v-card
+          class="mx-auto"
+          width="1200"
+          height="400">
+
+        <template v-slot:title>
+          This is a title
+        </template>
+
+        <v-card-text>
+          This is content
+        </v-card-text>
+      </v-card>
+    </div>
+
+    <div class="pokiCard">
+      <v-card
+          class="mx-auto"
+          width="1200"
+          height="400">
+
+        <template v-slot:title>
+          This is a title
+        </template>
+
+        <v-card-text>
+          This is content
+        </v-card-text>
+      </v-card>
+    </div>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "BuyCardView",
+  data: () => ({}),
+}
+</script>
+
+<style scoped>
+.picture {
+  background: url("../assets/poki.png") no-repeat center center fixed;
+  background-size: cover;
+  position: static;
+  overflow-x: hidden;
+}
+
+.pokiCard {
+  padding-top: 10px;
+}
+</style>
