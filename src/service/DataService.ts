7c967b30536749cd10146ea78c311359a2dd23b6
Melisa <melcafidan@gmail.com>
2022-09-21 11:39:21 +0200

    Get cards/decks/currency

--- src/service/DataService.ts
+++ src/service/DataService.ts
@@ -1,11 +1,30 @@
 import axios from "axios"
+import type {CurrencyRequest} from "@/requests/CurrencyRequest";
 
 const api_base_url = "http://localhost:8082/api"
 
 class DataService {
     async getCards() {
-        let value = axios.get(api_base_url + "/getAllCards")
+        let value = await axios.get(api_base_url + "/get-cards")
         console.log(value)
+
+        return value
+    }
+
+    async getDecks() {
+        let value = await axios.get(api_base_url + "/get-decks")
+        console.log(value)
+
+        return value
+    }
+
+    async getCurrency(currencyRequest: CurrencyRequest) {
+        let value = await axios.get(api_base_url + "/get-currency", {
+            params: {
+                ...currencyRequest
+            }
+        })
+        console.log("uuumm:" + value)
 
         return value
     }
